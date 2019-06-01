8f6ef206f403afe62cac5f9be57ac944224a6f2c
Mrzarabi <m.hadi.z.1997@gamil.com>
2019-06-01 17:31:07 +0430

    Dalete the extra migrations & models & factories & requests

--- database/factories/BrandFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(App\Models\Brand::class, function (Faker $faker) {
-    return [
-        'title' => $faker->name()
-    ];
-});
--- database/factories/SpecDataFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(App\Models\Spec\SpecData::class, function (Faker $faker) {
-    return [
-        
-    ];
-});
