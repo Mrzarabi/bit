a3356ff3005ba7536bcb67035b2f7d1e95f65004
Mrzarabi <m.hadi.z.1997@gamil.com>
2019-06-26 20:43:21 +0430

    re write some new factory

--- database/factories/CurrencyFactory.php
+++ database/factories/CurrencyFactory.php
@@ -5,10 +5,13 @@ use Faker\Generator as Faker;
 $factory->define(App\Models\Currency\Currency::class, function (Faker $faker) {
     $faker = \Faker\Factory::create('fa_IR');
     return [
-        'title'         => $faker->name(),
-        'description'   => nullable($faker->realText(255)),
-        'price'         => $faker->numberBetween(1000, 500000),
-        'inventory'     => $faker->numberBetween(1, 500000),
-        'image'         => nullable($faker->imageUrl($width = 640, $height = 480))
+        'title'                 => $faker->name(),
+        'short_description'     => nullable($faker->realText(255)),
+        'short_description'     => nullable($faker->realText(255)),
+        'price'                 => $faker->numberBetween(1000, 500000),
+        'inventory'             => $faker->numberBetween(1, 500000),
+        'status'                => $faker->numberBetween(0, 1),
+        'photo'                 => nullable($faker->imageUrl($width = 640, $height = 480)),
+        'code'                  => $faker->numberBetween(1, 500000)
     ];
 });
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,26 +16,31 @@ use Faker\Generator as Faker;
 $factory->define(App\User::class, function (Faker $faker) {
     $faker = \Faker\Factory::create('fa_IR');
     return [
-        'first_name'        => $faker->firstName(),
-        'second_name'       => $faker->firstName(),
-        'last_name'         => $faker->lastName(),
+        'first_name'                 => $faker->firstName(),
+        'last_name'                  => $faker->lastName(),
 
-        'social_link'       => $faker->numberBetween(00000, 99999),
-        'phone_number'      => $faker->phoneNumber,
-        'birthday'          => $faker->dateTimeThisCentury->format('Y-m-d'),
-        'address'           => $faker->address,
+        'national_code'              => $faker->numberBetween(00000, 99999),
+        'phone_number'               => $faker->phoneNumber,
+        'birthday'                   => $faker->dateTimeThisCentury->format('Y-m-d'),
+        'address'                    => $faker->address,
 
-        'email'             => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password'          => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'email'                      => $faker->unique()->safeEmail,
+        'email_verified_at'          => now(),
+        'password'                   => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         
-        'avatar'            => nullable($faker->imageUrl(50, 50)),
+        'avatar'                     => nullable($faker->imageUrl(50, 50)),
+        'type'                       => rand(0, 1),
 
-        'image_social_link' => $faker->imageUrl(50, 50),
-        'image_certificate' => $faker->imageUrl(50, 50),
-        'image_bill'        => $faker->imageUrl(50, 50),
-        'image_selfie_social_link' => $faker->imageUrl(50, 50),
+        'image_national_code'        => $faker->imageUrl(50, 50),
+        'identify_certificate'       => $faker->imageUrl(50, 50),
+        'image_bill'                 => $faker->imageUrl(50, 50),
+        'image_selfie_national_code' => $faker->imageUrl(50, 50),
 
-        'remember_token'    => str_random(10),
+        'accept_image_national_code' => $faker->boolean(),
+        'accept_identify_certificate'=> $faker->boolean(),
+        'accept_image_bill'         => $faker->boolean(),
+        'accept_image_selfie_national_code' => $faker->boolean(),
+
+        'remember_token'             => str_random(10),
     ];
 });
