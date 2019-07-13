6ffb6c1b7961a76c52dc92d6433f276570815818
Mrzarabi <m.hadi.z.1997@gamil.com>
2019-07-13 13:03:34 +0430

    add some related package like laratrust & taggable

---
+++ config/laratrust_seeder.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'role_structure' => [
+        'owner' => [
+            'category' => 'c,r,u,d',
+            'subject' => 'c,r,u,d',
+            'spec' => 'r,u',
+            'spec_header' => 'r,u',
+            'spec_row' => 'r,u',
+            'spec_data' => 'r,u',
+            '' => 'r,u',
+            'spec' => 'r,u',
+            'spec' => 'r,u',
+            'spec' => 'r,u',
+        ],
+    ],
+    'permissions_map' => [
+        'c' => 'create',
+        'r' => 'read',
+        'u' => 'update',
+        'd' => 'delete'
+    ]
+];
