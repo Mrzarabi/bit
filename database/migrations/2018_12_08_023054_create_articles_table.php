52507295f00e158ec421c93122a8b605e993aa16
Mrzarabi <m.hadi.z.1997@gamil.com>
2019-07-14 12:45:23 +0430

    make some new change

--- database/migrations/2018_12_08_023054_create_articles_table.php
+++ database/migrations/2018_12_08_023054_create_articles_table.php
@@ -27,13 +27,13 @@ class CreateArticlesTable extends Migration
             $table->increments('id')->unsigned();
 
             $table->foreign_key('user_id', 'users');
-            $table->foreign_key('subject_id', 'subjects');
+            $table->foreign_key('subject_id', true, 'subjects');
                     
             $table->string('title', 50);
             $table->string('slug', 50);
             $table->string('description', 255)->nullable();
             $table->text('body');
-            $table->string('image', 100)->nullable();
+            $table->string('image', 100);
 
             $table->full_timestamps();
         });
