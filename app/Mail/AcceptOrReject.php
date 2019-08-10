cbebd1b0ad85b41356d1822301a7fbbe486b6030
Mrzarabi <m.hadi.z.1997@gamil.com>
2019-08-10 08:42:57 +0430

    use laravel mail

---
+++ app/Mail/AcceptOrReject.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class AcceptOrReject extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $request;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('support@mashadexchange.ir')
+                ->markdown('emails.users.accet_or_reject');
+    }
+}
