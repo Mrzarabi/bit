f47debad859e3a6005f32ee715786e323b4fa60c
Mrzarabi <m.hadi.z.1997@gamil.com>
2019-08-19 18:46:16 +0430

    delete the extra code

--- app/Http/Requests/V1/Grouping/SubjectRequest.php
+++ app/Http/Requests/V1/Grouping/SubjectRequest.php
@@ -27,7 +27,7 @@ class SubjectRequest extends FormRequest
             'title'           => 'required|string|max:50',
             'description'     => 'nullable|string|max:255',
             'logo'            => [
-                request()->method() === 'POST' ? 'required' : 'nullable', 'image', 'mimes:jpeg,jpg,png,gif', 'max:1024'
+                'nullable', 'image', 'mimes:jpeg,jpg,png,gif', 'max:1024'
             ],
         ];
     }
