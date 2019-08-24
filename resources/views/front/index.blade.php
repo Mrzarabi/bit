b99a6c56ceedaf1701b53f865b5eea1160d9478f
Mrzarabi <m.hadi.z.1997@gamil.com>
2019-08-24 17:27:46 +0430

    add real text

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -44,25 +44,32 @@
     </div> --}}
     <!--  /.bitcoin-price-section -->
     <!--  about-section -->
-    <div class="space-medium">
+    <div class="space-medium" id="rules">
         <div class="container mt">
             <div class="row">
                 <!--  section-title -->
                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 ">
                     <div class="section-title">
-                        <h2 class="text-right mt-100">لورم ایپسوم</h2>
+                        <h2 class="text-right mt-100">قوانین </h2>
                     </div>
                 </div>
                 <!--  /.section-title -->
             </div>
             <!-- about -->
             <div class="row text-right">
-                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 ">
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 ">
                     <div class="mb20">
-                        <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. </p>
+                        <ul>
+                            <li> <p class="lead"> کلیه فعالیت های سایت مشهد اکسچنج تابع قوانین جمهوری اسلامی ایران به صورت عام و جرایم رایانه ای به صورت خاص می باشد  </p> </li>
+                            <li> <p> براساس تکلیفی که قوانین جرایم رایانه ای و همپنین پلیس فتا بر عهده فروشندگان در فضای مجازی قرار داده سایت مشهد اکسچنج خود را ملزم به احراز هویت متقاضیان خرید و فروش می داند .  </p> </li>
+                            <li> <p> در صورت تلاش جهت استفاده از کارتهای بانکی سرقتی و یا اطلاعات هک شده دیگران در هنگام خرید ، کلیه اطلاعات و مشخصات شما در اختیار پلیس فتا قرار داده خواهد شد .  </p> </li>
+                            <li> <p> واریز به حساب شخص ثالث طبق قوانین جمهوری اسلامی و بانک مرکزی ممنوع می باشد لذا واریز کننده وجه و درخواست کننده سفارش باید یکسان باشد. </p> </li>
+                            <li> <p> در صورت ثبت نادرست حساب گیرنده و عدم اطمینان از گیرنده ، سایت مشهد اکسچنج هیچگونه مسئولیتی در این راستا نمی پذیرد .  </p> </li>
+                            <li> <p> ثبت نام و ثبت سفارش در سایت کاربر تایید می کند که موارد فوق را مطالعه و پذیرفته است .  </p> </li>
+                        </ul>
                     </div>
                 </div>
-                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 ">
+                {{-- <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 ">
                     <div class="mb20">
                         <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. </p>
                     </div>
@@ -71,7 +78,7 @@
                     <div class="mb20">
                         <p class="lead">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. </p>
                     </div>
-                </div>
+                </div> --}}
             </div>
             <!-- /.about description -->
             <hr>
@@ -105,15 +112,14 @@
     </div>
     <!-- /.about -->
     <!--  buy/sell-section -->
-    <div class="space-medium bg-light">
+    <div class="space-medium bg-light" id="about">
         <div class="container">
             <div class="row ">
                 <!--  buy/sell-details -->
                 <div class="offset-xl-1 col-xl-5 col-lg-6 col-md-12 col-sm-12 col-12">
                     <div class="pdt40 text-right">
-                        <h2>خرید و فروش ارز دیجیتال</h2>
-                        <p class="lead">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. </p>
-                        <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. </p>
+                        <h3> خرید و فروش انواع ارزهای الکترونیکی و دیجیتال </h3>
+                        <p> سایت مشهد اکسچنج با هدف دور زدن تحریم های مالی و کم کردن آثار آن در زمینه تامین ارز های مجازی برای هموطنان جهت معاملات بین المللی و نقد کردن درآمد های ارزی شما فعال می باشد . </p>
                         @guest
                             @if (Route::has('register'))
                             <div class="row">
@@ -223,40 +229,53 @@
         <div class="container">
             <div class="row text-right">
                 <!-- feature-left -->
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
+                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
                     <div class="feature-left">
                         <div class="feature-icon float-right">
-                            <img src="/images/bitcoin/icons/padlock.png" class="custom-icons" alt="تصویر">
+                            <img src="/images/bitcoin/icons/reload.png" class="custom-icons" alt="تصویر">
                         </div>
                         <div class="feature-content">
-                            <h4>امنیت</h4>
-                            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. </p>
+                            <h4>به روز رسانی</h4>
+                            <p> به روز رسانی نرخ ها بصورت روزانه </p>
                         </div>
                     </div>
                 </div>
                 <!-- /.feature-left -->
                 <!-- feature-left -->
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
+                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
+                    <div class="feature-left">
+                        <div class="feature-icon float-right">
+                            <img src="/images/bitcoin/icons/choices.png" class="custom-icons" alt="تصویر">
+                        </div>
+                        <div class="feature-content">
+                            <h4>سفارشات</h4>
+                            <p> دریافت سفارش در کمترین زمان ممکن </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.feature-left -->
+                <!-- feature-left -->
+                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
                     <div class="feature-left">
                         <div class="feature-icon float-right">
                             <img src="/images/bitcoin/icons/call-center.png" class="custom-icons" alt="تصویر">    
                         </div>
                         <div class="feature-content">
                             <h4>پشتیبانی</h4>
-                            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. </p>
+                            <p> پشتیبانی با کیفیت از طریق تیکت </p>
                         </div>
                     </div>
                 </div>
                 <!-- /.feature-left -->
                 <!-- feature-left -->
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
+                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
                     <div class="feature-left float-right">
                         <div class="feature-icon">
                             <img src="/images/bitcoin/icons/wallet.png" class="custom-icons" alt="تصویر">    
                         </div>
                         <div class="feature-content">
                             <h4>کیف پول</h4>
-                            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. </p>
+                            <p> مناسب ترین نرخ خرید و فروش </p>
                         </div>
                     </div>
                 </div>
@@ -272,8 +291,8 @@
                 <div class="col-xl-6col-lg-6 col-md-12 col-sm-12 col-12">
                     {{-- <div style="width: 100%; height: 100%; position: relative; top: 0px; bottom: 0px;">  --}}
                         <div class="cta-block">
-                            <h1 class="text-white">لورم ایپسوم متن ساختگی با تولید سادگی.</h1>
-                            <p>لورم ایپسوم متن ساختگی با تولید سادگی.</p>
+                            <h2 class="text-white text-shadow-custom">سادگی و امنیت خاطر را با ما تجربه کنید</h2>
+                            {{-- <p>سادگی و امنیت خاطر را با ما تجربه کنید</p> --}}
                             {{-- <a href="#" class="btn btn-default custom-btn-warning">همین حالا ثبت نام کنید</a> --}}
                         </div>
                     {{-- </div> --}}
@@ -325,7 +344,7 @@
         {
             "particles": {
                 "number": {
-                    "value": 80,
+                    "value": 20,
                     "density": {
                         "enable": true,
                         "value_area": 800
@@ -350,7 +369,7 @@
                     }
                 },
                 "opacity": {
-                    "value": 0.5,
+                    "value": 0.4,
                     "random": false,
                     "anim": {
                         "enable": false,
@@ -378,7 +397,7 @@
                 },
                 "move": {
                     "enable": true,
-                    "speed": 6,
+                    "speed": 1,
                     "direction": "none",
                     "random": false,
                     "straight": false,
